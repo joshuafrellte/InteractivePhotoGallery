b273f84e135e0674c50f24a7771cfdb4cd5e0230
joshuafrellte <joshuafrellt@gmail.com>
2025-11-19 18:52:28 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+function upDate(previewPic) {
+    displayBox = document.getElementById('display_box');
+    displayBox.innerHTML = previewPic.alt;
+    displayBox.style.backgroundImage = "url('"+previewPic.src+"')";
+    displayBox.style.backgroundSize = "cover";
+    displayBox.style.borderColor = "white";
+    displayBox.style.boxShadow = "0 0 20px white";
+    displayBox.style.animation = "none";
+    displayBox.style.transition = "0.15s ease";
+    displayBox.style.transform = "scale(1.1)";
+    console.log("On Display: "+previewPic.alt);
+}
+
+function unDo() {
+    displayBox = document.getElementById('display_box');
+    displayBox.innerHTML = "Hover over an image below to display here";
+    displayBox.style.backgroundImage = "radial-gradient(black, grey, white)";
+    displayBox.style.borderColor = "black";
+    displayBox.style.boxShadow = "0 0 20px black";
+    displayBox.style.animation = "2s breathe ease-out alternate infinite";
+    displayBox.style.transform = "scale(1.0)";
+}
+
+function assignTabIndex() {
+    images = document.querySelectorAll('img');
+    for (let i=0; i<images.length; i++) {
+        // images[i].tabIndex = 0;
+        images[i].setAttribute("tabindex","0");
+        console.log(images[i].alt+" tabindex: " + images[i].tabIndex);
+    }
+}
+
